fc34453b1bf61ad846979dc61801160b7f130795
Robin Leboe <robin.leboe@gmail.com>
2019-12-12 11:54:12 -0800

    fix typo in comments.js

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -28,24 +28,24 @@ module.exports = {
         return post;
       } else throw new UserInputError('Post not found');
     },
-    deleteComment: async (_,{ postId, commentId}, context) => {
-        const { username } = checkAuth(context);
+    deleteComment: async (_, { postId, commentId }, context) => {
+      const { username } = checkAuth(context);
 
-        const post = await Post.findById(postId);
+      const post = await Post.findById(postId);
 
-        if(post){
-            const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+      if (post) {
+        const commentIndex = post.comments.findIndex(c => c.id === commentId);
 
-            if(post.comments[commentIndex].username === username){
-                post.comments.splice(commentIndex, 1);
-                await post.save();
-                return post;
-            } else {
-                throw new AuthenticationError('Action not allowed');
-            }
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
         } else {
-            throw new UserInputError('Post not found');
+          throw new AuthenticationError('Action not allowed');
         }
+      } else {
+        throw new UserInputError('Post not found');
+      }
     }
   }
 };
