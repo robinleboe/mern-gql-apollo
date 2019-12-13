c5da38582f70278f6a1175219a253ac274d2cab5
Robin Leboe <robin.leboe@gmail.com>
2019-12-12 20:59:33 -0800

    Add body content validation to createPost

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -31,6 +31,10 @@ module.exports = {
       const user = checkAuth(context);
       console.log(user);
 
+      if(arguments.body.trim() === '') {
+        throw new Error('Post body must not be empty');
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
